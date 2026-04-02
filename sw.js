b9c0c17287637e5e1222d7cfbb8ec8505fca37c8
YK <yusukeft@yahoo.co.jp>
2026-04-02 09:52:26 +0900

    今日追加した修正提案をすべて元に戻す (Revert to state before April 2)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'glass-music-player-v3';
+const CACHE_NAME = 'glass-music-player-v1';
 const urlsToCache = [
   './',
   './index.html',
@@ -10,7 +10,6 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting(); // 強制的に新しいバージョンをアクティブにする
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -35,7 +34,6 @@ self.addEventListener('fetch', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim()); // 即座にコントロールを奪う
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
